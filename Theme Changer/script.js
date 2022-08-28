766fdd06d11dd43afb941abea201a4a35681e984
Muskaan Lakhina <muskaanlakhina10@gmail.com>
2022-08-28 20:57:36 +0530

    Nagarro All projects updated

---
+++ Theme Changer/script.js
@@ -0,0 +1,24 @@
+function setblack() {
+    document.getElementById("contentid").style.backgroundColor = "black";
+    document.getElementById("textid").style.borderColor = "white";
+    document.getElementById("textid").style.backgroundColor = "black";
+    document.getElementById("textid").style.color = "white";
+  }
+  function setlavender() {
+      document.getElementById("contentid").style.backgroundColor = "rgb(244,144,238)";
+      document.getElementById("textid").style.borderColor = "black";
+      document.getElementById("textid").style.backgroundColor = "rgb(244,144,238)";
+      document.getElementById("textid").style.color = "black";
+  }
+  function setgreen() {
+      document.getElementById("contentid").style.backgroundColor = "green";
+      document.getElementById("textid").style.borderColor = "black";
+      document.getElementById("textid").style.backgroundColor = "green";
+      document.getElementById("textid").style.color = "black";
+  }
+  function setmaroon() {
+      document.getElementById("contentid").style.backgroundColor = "maroon";
+      document.getElementById("textid").style.borderColor = "white";
+      document.getElementById("textid").style.backgroundColor = "maroon";
+      document.getElementById("textid").style.color = "white";
+  }
